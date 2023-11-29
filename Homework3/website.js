629bb736325d0977f20985539d5b25d8bf92b2ab
speyec <ouatum44@gmail.com>
2023-11-29 14:06:48 -0500

    i added the website that I made

---
+++ Homework3/website.js
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Web Portfolio</title>
+        <link type="text/css" rel="stylesheet" href="styles/my_style.css" />
+    </head>
+    <body>
+        <div class="border">
+            <div class="banner">
+        <h1>Web Portfolio</h1>
+        <nav>
+        <ul> 
+            <li><a href="index.html">Home</a></li>
+            <li><a href="accessibility.html">Accessibility</a></li>
+            <li><a href="javaScript.html">Javascript</a></li>
+            <li><a href="react.html">React</a></li>
+        </ul>
+        </nav>
+    </div>
+        <ul>
+            <li class="purple_italic_font">Michael Ouatu</li>
+            <li><a class="purple_italic_font" href= "mailto:mouatu@villanova.edu">Email</a></li>
+            <li><a class="purple_italic_font" href = "http:www.villanova.edu">Villanova University</a></li>
+        </ul>
+        <p id="green_font">Learning Web Development and Advancing My Skills</p>
+    </div>
+    </body>
+</html>
